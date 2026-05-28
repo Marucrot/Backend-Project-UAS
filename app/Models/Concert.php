2737270af5fd0d850e8bb203e6b544111e9940f3
Apocketfullofaces <44thstring@gmail.com>
2026-05-28 17:34:50 +0700

    Fixed model concert and artist, added controller, added concerts and artists in routes

--- app/Models/Concert.php
+++ app/Models/Concert.php
@@ -7,5 +7,37 @@ use Illuminate\Database\Eloquent\Model;
 
 class Concert extends Model
 {
-    // 
+    use HasFactory;
+ 
+    protected $fillable = [
+        'name',
+        'description',
+        'poster_url',
+        'event_date',
+        'status',
+    ];
+ 
+    protected $casts = [
+        'event_date' => 'datetime',
+    ];
+ 
+    public function artists()
+    {
+        return $this->belongsToMany(Artist::class, 'concert_artist');
+    }
+ 
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+ 
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
+ 
+    public function wishlists()
+    {
+        return $this->hasMany(Wishlist::class);
+    }
 }
